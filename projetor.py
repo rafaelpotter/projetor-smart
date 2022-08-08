ba617911cda99403f704a7eb04b647ef3c9a2f98
Gabriel Hartmann de Azeredo <gabriel.hazeredo@gmail.com>
2022-08-08 16:09:22 -0300

    aquisição de vídeo da webcam

--- projetor.py
+++ projetor.py
@@ -1,5 +1,23 @@
-def main():
-    print("Init")
+import numpy as np
+import cv2
+
+def read_webcam():
+    cap = cv2.VideoCapture('data\laser_tests_trim.mp4') # use (0) for webcam
+
+    while True:
+        ret, frame = cap.read()
+
+        cv2.imshow('frame', frame)
+
+        blank_Image = np.zeros(frame.shape, np.uint8)
+        cv2.imshow('blank_Image', blank_Image)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    main()
+    read_webcam()
+    
