0a291bbc8f521928e3814f15c3c5b62491a958c9
Gabriel Hartmann de Azeredo <gabriel.hazeredo@gmail.com>
2022-08-09 10:13:42 -0300

    error handling

--- projetor.py
+++ projetor.py
@@ -1,25 +1,33 @@
 import numpy as np
 import cv2 as cv
 
-def read_webcam():
-    cap = cv.VideoCapture('data\laser_tests_trim.mp4') # use (0) for webcam
-    # Frames da webcam
-    ret, frame = cap.read()
+def read_webcam(webcam):
+    # webcam standard = 0
+    cap = cv.VideoCapture(webcam)
+
+    if not cap.isOpened():
+        print("Error opening video")
+
+    # size = (int(cap.get(3)), int(cap.get(4)))
+    # print(size)
+    status, frame = cap.read()
     blank = np.zeros(frame.shape, dtype='uint8')
 
-    while True:
-        ret, frame = cap.read()
-        # Transformar para escala de cinza e aplicar threshold
-        # Analisar histograma de testes para validar melhor threshold
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
+    while (cap.isOpened()):
+        # Frames da webcam
+        status, frame = cap.read()
+        if status:
+            # Transformar para escala de cinza e aplicar threshold
+            # Analisar histograma de testes para validar melhor threshold
+            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            threshold, thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
 
-        # Achar contornos
-        contours, hier = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+            # Achar contornos
+            contours, hier = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 
-        # Desenhar contornos em canvas escuro
-        contours_generated = cv.drawContours(blank, contours, -1, (0,0,255), -1)
-        cv.imshow('contours blank', contours_generated)
+            # Desenhar contornos em canvas escuro
+            contours_generated = cv.drawContours(blank, contours, -1, (0,0,255), -1)
+            cv.imshow('contours blank', contours_generated)
 
         if cv.waitKey(1) == ord('q'):
             break
@@ -28,5 +36,5 @@ def read_webcam():
     cv.destroyAllWindows()
 
 if __name__ == "__main__":
-    read_webcam()
+    read_webcam('standard_data\laser_emulation.mp4')
     
